1a1b498b4e4d5b6b99c6a9f001f8c40927067289
ClaytonSiman <clayton2131@gmail.com>
2025-03-09 13:30:41 +0800

    Add ISYE 6501 files

---
+++ ISYE 6501 Intro to Analytics Modelling/Module 13 Probability-based Models/Homework 9.R
@@ -0,0 +1,7 @@
+library(FrF2)
+
+FrF2(
+  16, 
+  factor.names = c("large_yard", "solar_roof", "large_garage", "more_than_2_bathrooms", "more_than_3_bedrooms", "multiple_floors", "near_public_transport", "quiet_neighbourhood", "marble_flooring", "less_than_10_years_old"), 
+  default.levels = c("yes", "no")
+)
