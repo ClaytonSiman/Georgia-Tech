0977978d10fa448222ac6e8f10818413baa32fa4
ClaytonSiman <clayton2131@gmail.com>
2025-09-17 22:06:50 +0800

    Add Homework 2 for DVA

--- CSE 6242 Data & Visual Analytics/Homework 1/Q5/wrangling_scripts/Q5.py
+++ CSE 6242 Data & Visual Analytics/Homework 1/Q5/wrangling_scripts/Q5.py
@@ -8,7 +8,7 @@ import csv
 
 
 def username():
-    return 'gburdell3'
+    return 'csiman6'
 
 
 def data_wrangling(filter_class: str = None):
@@ -21,27 +21,28 @@ def data_wrangling(filter_class: str = None):
         reader = csv.reader(f)
         table = list()
         # Feel free to add any additional variables
-        ...
-        
-        # Read in the header
-        for header in reader:
-            break
+        header = next(reader)
+
+        class_set = set()
         
         # Read in each row
         for row in reader:
             row_data = [row[0], row[1], int(row[2])]
+            class_set.add(row[1])
             table.append(row_data)
         
         # Programmatically get unique classes and sort alphabetically for dropdown - [2 point] Q5.4.a
-        dropdown_options = []
+        dropdown_options = list(class_set)
+        dropdown_options.sort()
         
         # Filter, sort, and limit the table - [3 points] Q5.4.b
         # Filter the data by the class column (second column)
         if filter_class:
-            ...
+            table = [row for row in table if (row[1] == filter_class or filter_class is None)]
         # Order table by the count column (last column) - don't need to worry about tiebreaks
-        ...
+        table = sorted(table, key=lambda x: -x[2])
         # Take only the first 10 rows
-        ...
+        table = table[:10]
+        
     
     return header, table, dropdown_options
